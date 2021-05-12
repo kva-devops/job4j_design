b85b0940440fd4f41d95b543ab35ba88d0ece998
vladimirKutyavin <kva.devops@gmail.com>
2021-05-12 21:04:35 +0400

    Init travis & jacoco

--- src/main/java/ru/job4j/App.java
+++ src/main/java/ru/job4j/App.java
@@ -6,8 +6,7 @@ package ru.job4j;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main(String[] args) {
+        System.out.println("Hello World");
     }
 }
