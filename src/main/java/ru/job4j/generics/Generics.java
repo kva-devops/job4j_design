62672a607ba71ba398e45494e621b7880624475b
vladimirKutyavin <kva.devops@gmail.com>
2021-05-16 11:19:58 +0400

    0. Что такое обобщенные типы (generics) [#533226]

---
+++ src/main/java/ru/job4j/generics/Generics.java
@@ -0,0 +1,52 @@
+package ru.job4j.generics;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Generics {
+    public static void main(String[] args) {
+        Generics gen = new Generics();
+        List<Animal> first = new ArrayList<>();
+        List<Predator> second = new ArrayList<>();
+        List<Tiger> third = new ArrayList<>();
+        first.add(new Animal());
+        second.add(new Predator());
+        third.add(new Tiger());
+
+        gen.printObjet(first);
+        gen.printObjet(second);
+        gen.printObjet(third);
+        System.out.println();
+
+        //gen.printBoundedWildCard(first);
+        gen.printBoundedWildCard(second);
+        gen.printBoundedWildCard(third);
+        System.out.println();
+
+        gen.printLowerBoundedWildCard(first);
+        gen.printLowerBoundedWildCard(second);
+        //gen.printLowerBoundedWildCard(third);
+    }
+
+    public void printObjet(List<?> list) {
+        for (Iterator<?> it = list.iterator(); it.hasNext();) {
+            Object next = it.next();
+            System.out.println("Current element: " + next);
+        }
+    }
+
+    public void printBoundedWildCard(List<? extends Predator> list) {
+        for (Iterator<? extends Predator> it = list.iterator(); it.hasNext();) {
+            Object next = it.next();
+            System.out.println("Current element: " + next);
+        }
+    }
+
+    public void printLowerBoundedWildCard(List<? super Predator> list) {
+        for (Iterator<? super Predator> it = list.iterator(); it.hasNext();) {
+            Object next = it.next();
+            System.out.println("Current element: " + next);
+        }
+    }
+}
