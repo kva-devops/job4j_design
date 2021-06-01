92e3186d7ed37be6b61fc9f280db80b588eda84a
vladimirKutyavin <kva.devops@gmail.com>
2021-06-01 15:54:01 +0400

    2. Анализ доступности сервера. [#859]

---
+++ src/main/java/ru/job4j/io/Analise.java
@@ -0,0 +1,53 @@
+package ru.job4j.io;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Analise {
+    public void unavailable(String source, String target) {
+        List<String> timeList = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(source))) {
+            String checkNull;
+            String checkBefore = "";
+            StringBuilder sb = new StringBuilder();
+            do {
+                String buff = br.readLine();
+                checkNull = buff;
+                if (buff != null) {
+                    String[] buffArr = buff.split(" ");
+                    if (("400".equals(buffArr[0]) || "500".equals(buffArr[0])) && !"400".equals(checkBefore) && !"500".equals(checkBefore)) {
+                        sb.append(buffArr[1]);
+                        checkBefore = buffArr[0];
+                    }
+                    if ("".equals(checkBefore)) {
+                        checkBefore = buffArr[0];
+                    }
+                    if (("400".equals(checkBefore) || "500".equals(checkBefore)) && ("200".equals(buffArr[0]) || "300".equals(buffArr[0]))) {
+                        sb.append(";");
+                        sb.append(buffArr[1]);
+                        checkBefore = buffArr[0];
+                        timeList.add(sb.toString());
+                        sb.setLength(0);
+                    }
+                }
+            } while (checkNull != null);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        try (PrintWriter out = new PrintWriter(new BufferedOutputStream(new FileOutputStream(target)))) {
+            for (String s : timeList) {
+                out.write(s + System.lineSeparator());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        Analise analise = new Analise();
+        String pathLog = "./data/server.log";
+        String pathResult = "./unavailable.csv";
+        analise.unavailable(pathLog, pathResult);
+    }
+}
