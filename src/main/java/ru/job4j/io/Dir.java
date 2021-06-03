faee42e1aa76c248c6f41c548abcd603cf2a9002
vladimirKutyavin <kva.devops@gmail.com>
2021-06-03 15:41:44 +0400

    4.0. File [#252491 #163294] - correcting

--- src/main/java/ru/job4j/io/Dir.java
+++ src/main/java/ru/job4j/io/Dir.java
@@ -3,6 +3,9 @@ package ru.job4j.io;
 import java.io.File;
 
 public class Dir {
+    private static final int GB = 1073741824;
+    private static final int KB = 1024;
+
     public static void main(String[] args) {
         File file = new File("/home/kustoff/projects/");
         if (!file.exists()) {
@@ -11,9 +14,9 @@ public class Dir {
         if (!file.isDirectory()) {
             throw new IllegalArgumentException(String.format("Not directory %s", file.getAbsoluteFile()));
         }
-        System.out.println(String.format("Total disk size : %s Gb", file.getTotalSpace() / 1073741824));
+        System.out.println(String.format("Total disk size : %s Gb", file.getTotalSpace() / GB));
         for (File subfile : file.listFiles()) {
-            System.out.println(subfile.getName() + " : " + subfile.length() / 1024 + "Kb");
+            System.out.println(subfile.getName() + " : " + subfile.length() / KB + "Kb");
         }
     }
 }
