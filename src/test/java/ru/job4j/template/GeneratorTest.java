da0bf1acf0b68d7d87dd8e02540559331386e0f1
vladimirKutyavin <kva.devops@gmail.com>
2021-07-12 10:51:25 +0400

    3. Шаблонизатор. [#855]

---
+++ src/test/java/ru/job4j/template/GeneratorTest.java
@@ -0,0 +1,43 @@
+package ru.job4j.template;
+
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class GeneratorTest {
+
+    @Test
+    public void produce() {
+        Generator generator = new GeneratorStr();
+        Map<String, String> mapKeys = new HashMap<>();
+        mapKeys.put("key1", "value1");
+        String templateStr = "This is ${key1}";
+        String expected = "This is value1";
+        String result = generator.produce(templateStr, mapKeys);
+        assertThat(result, is(expected));
+    }
+
+    @Test(expected = Exception.class)
+    public void notKeyInMap() {
+        Generator generator = new GeneratorStr();
+        Map<String, String> mapKeys = new HashMap<>();
+        mapKeys.put("key1", "value1");
+        String templateStr = "This is ${key2}";
+        generator.produce(templateStr, mapKeys);
+    }
+
+    @Test(expected = Exception.class)
+    public void moreKeyInMap() {
+        Generator generator = new GeneratorStr();
+        Map<String, String> mapKeys = new HashMap<>();
+        mapKeys.put("key1", "value1");
+        mapKeys.put("key2", "value2");
+        String templateStr = "This is ${key1}";
+        generator.produce(templateStr, mapKeys);
+    }
+
+}
