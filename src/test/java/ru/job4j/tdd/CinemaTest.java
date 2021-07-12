9b00afc0a13fe5fe5c8ee2530fb62d505f9ded83
vladimirKutyavin <kva.devops@gmail.com>
2021-07-12 14:48:35 +0400

    2. Что такое TDD? [#4918 #176674] - add test error

--- src/test/java/ru/job4j/tdd/CinemaTest.java
+++ src/test/java/ru/job4j/tdd/CinemaTest.java
@@ -36,4 +36,34 @@ public class CinemaTest {
         cinema.add(sessionInit);
         assertThat(sessionInit, is(cinema.find(session -> true).get(0)));
     }
+
+    @Test(expected = Exception.class)
+    public void buyNotCorrectDate() {
+        Account account = new Account3D();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2020, 13, 30, 23, 00);
+        Ticket ticket = cinema.buy(account, 1, 1, date);
+    }
+
+    @Test(expected = Exception.class)
+    public void buyOnePlaceError() {
+        Account account1 = new Account3D();
+        Account account2 = new Account3D();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2020, 10, 30, 23, 00);
+        Ticket ticket1 = cinema.buy(account1, 1, 1, date);
+        Ticket ticket2 = cinema.buy(account2, 1, 1, date);
+    }
+
+    @Test(expected = Exception.class)
+    public void buyErrorPlace() {
+        Account account = new Account3D();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2020, 10, 30, 23, 00);
+        Ticket ticket = cinema.buy(account, 111, 222, date);
+    }
+
 }
